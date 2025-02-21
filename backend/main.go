069ebf459574811644e41b0397e369f78ac61190
Ezhil Shanmugham <ezhil930@gmail.com>
2025-02-21 11:26:59 +0530

    feat: cors

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,7 @@ import (
 	"task-manager/models"
 	"task-manager/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,13 @@ func main() {
 	db.DB.AutoMigrate(&models.User{}, &models.Task{})
 
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"*"},
+	}))
+
 	routes.RegisterRoutes(server)
 	server.Run(":8080")
 }
